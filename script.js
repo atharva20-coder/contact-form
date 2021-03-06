766a6bca06521f70e78ead806c7a5e3aa667a4c2
Atharva Joshi <69634375+atharva20-coder@users.noreply.github.com>
2021-03-06 15:03:17 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const form = document.getElementById('form');
+/*const form = document.getElementById('form');
 
 form.addEventListener('submit', e => {
 	e.preventDefault();
@@ -52,3 +52,4 @@ function isValid(email) {
 	var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 	return re.test(String(email).toLowerCase());
 }
+*/
